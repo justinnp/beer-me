1dd29e3794cdd8849b0375d594163d6fb6135c0e
Navon Francis <navonfrancis@gmail.com>
2018-01-01 20:51:16 -0500

    search bar

--- src/App.js
+++ src/App.js
@@ -2,18 +2,27 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import TextField from 'material-ui/TextField';
+
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to BeerMe!</h1>
-        </header>
-        <p className="App-intro">
-          Please enter in your beer of choice:
-        </p>
-      </div>
+      <MuiThemeProvider>
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <h1 className="App-title">Welcome to BeerMe!</h1>
+          </header>
+          <p className="App-intro">
+            Please enter in your beer of choice:
+          </p>
+          <TextField
+            hintText="Enter a beer!"
+            floatingLabelText="beerMe!"
+            />
+        </div>
+      </MuiThemeProvider>
     );
   }
 }
